3ba94d1f449bdcf38d38592773d4c9989d4a12d1
Moshe W <pnemonic@gmail.com>
2026-01-15 10:08:39 +0200

    keep screen on

--- composeApp/src/androidMain/kotlin/pnemonic/bug_squash/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/pnemonic/bug_squash/MainActivity.kt
@@ -1,20 +1,30 @@
 package pnemonic.bug_squash
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import pnemonic.bug_squash.view.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
 
         setContent {
             App()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        keepScreenOn()
+    }
+
+    private fun keepScreenOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
