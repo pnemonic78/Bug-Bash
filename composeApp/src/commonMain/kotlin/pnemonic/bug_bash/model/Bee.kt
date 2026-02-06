a7becf91cca2d2af6632b65a5fa4477104ce8e67
Moshe W <pnemonic@gmail.com>
2026-02-06 13:43:28 +0200

    slower bugs

--- composeApp/src/commonMain/kotlin/pnemonic/bug_bash/model/Bee.kt
+++ composeApp/src/commonMain/kotlin/pnemonic/bug_bash/model/Bee.kt
@@ -2,6 +2,6 @@ package pnemonic.bug_bash.model
 
 import pnemonic.bug_bash.sound.SoundType
 
-class Bee : BugThatFlies(speed = 1f, score = 15, hits = 1, sound = SoundType.Squirt) {
+class Bee : BugThatFlies(speed = 0.5f, score = 15, hits = 1, sound = SoundType.Squirt) {
     override val description = "🐝"
 }
--- composeApp/src/commonMain/kotlin/pnemonic/bug_bash/model/Scorpion.kt
+++ composeApp/src/commonMain/kotlin/pnemonic/bug_bash/model/Scorpion.kt
@@ -2,6 +2,6 @@ package pnemonic.bug_bash.model
 
 import pnemonic.bug_bash.sound.SoundType
 
-class Scorpion : BugThatCrawls(speed = 0.5f, score = 15, hits = 5, sound = SoundType.SlimeSplash) {
+class Scorpion : BugThatCrawls(speed = 0.25f, score = 15, hits = 5, sound = SoundType.SlimeSplash) {
     override val description = "\uD83E\uDD82"
 }
