c3b2c3b3eeb92190ad96a0daa41de8d0e7245e71
Moshe W <pnemonic@gmail.com>
2026-02-15 21:51:32 +0200

    bug speed

--- composeApp/src/commonMain/kotlin/pnemonic/bug_bash/model/Caterpillar.kt
+++ composeApp/src/commonMain/kotlin/pnemonic/bug_bash/model/Caterpillar.kt
@@ -2,6 +2,6 @@ package pnemonic.bug_bash.model
 
 import pnemonic.bug_bash.sound.SoundType
 
-class Caterpillar : BugThatSlithers(speed = 0.25f, score = 5, hits = 3, soundBash = SoundType.Squeeze) {
+class Caterpillar : BugThatSlithers(speed = 0.2f, score = 5, hits = 3, soundBash = SoundType.Squeeze) {
     override val description = "\uD83D\uDC1B"
 }
